e3648ccec229a72d88912b8a43779000ac8ed6ac
Brittany Rausch <berausch247@gmail.com>
2016-07-24 18:07:50 -0700

    Create the Client Test for 'returns true for same name'

--- Tests/ClientTest.cs
+++ Tests/ClientTest.cs
@@ -13,5 +13,20 @@ namespace Salon
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
     }
 
+    [Fact]
+    public void Test_CategoriesEmptyAtFirst()
+    {
+      int result = Client.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+    [Fact]
+    public void Test_Equal_ReturnsTrueForSameName()
+    {
+      Client firstClient = new Client("Harry Styles");
+      Client secondClient = new Client("Harry Styles");
+
+      Assert.Equal(firstClient, secondClient);
+    }
   }
 }
